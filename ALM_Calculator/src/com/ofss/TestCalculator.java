37987ec7dcfd5ed463ff626468d3f375018fd5ff
ashwsaxe <ashwsaxe@ASHWSAXE-IN.in.oracle.com>
2019-04-16 12:19:26 +0530

    AshBranch Change1 committed.

--- ALM_Calculator/src/com/ofss/TestCalculator.java
+++ ALM_Calculator/src/com/ofss/TestCalculator.java
@@ -5,5 +5,6 @@ public class TestCalculator {
 	public static void main(String[] args) {
 		Calculator c = new Calculator();
 		System.out.println("Result::"+c.add(3,5));
+		System.out.println("AshBranch Result::"+c.add(2,5));
 	}
 }
