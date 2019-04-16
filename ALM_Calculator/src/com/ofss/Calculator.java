e1b207d7e6715df70fb11fb77dc2f98104017e8d
mansaka <mansaka@MANSAKA-IN.in.oracle.com>
2019-04-16 11:40:15 +0530

    First Commit.

---
+++ ALM_Calculator/src/com/ofss/Calculator.java
@@ -0,0 +1,22 @@
+package com.ofss;
+
+public class Calculator {
+	
+	int a;
+	int b;
+	
+	public Calculator() {
+		super();
+	}
+	
+	public Calculator(int a, int b) {
+		super();
+		this.a = a;
+		this.b = b;
+	}
+	
+	public int add(int a, int b) {		
+		return a+b;
+	}
+
+}
---
+++ ALM_Calculator/src/com/ofss/TestCalculator.java
@@ -0,0 +1,9 @@
+package com.ofss;
+
+public class TestCalculator {
+
+	public static void main(String[] args) {
+		Calculator c = new Calculator();
+		System.out.println("Result::"+c.add(3,5));
+	}
+}
