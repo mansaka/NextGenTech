0479bf8da0600001aeb6464234fed84ba7b54f7e
dkabbina <dkabbina@DKABBINA-PC2.in.oracle.com>
2019-04-16 12:11:39 +0530

    Subtraction Function Included.

--- ALM_Calculator/src/com/ofss/TestCalculator.java
+++ ALM_Calculator/src/com/ofss/TestCalculator.java
@@ -4,6 +4,6 @@ public class TestCalculator {
 
 	public static void main(String[] args) {
 		Calculator c = new Calculator();
-		System.out.println("Result::"+c.add(3,5));
+		System.out.println("Result::"+c.add(3,5)+" Subtraction Result::"+c.subtract(5,2));
 	}
 }
